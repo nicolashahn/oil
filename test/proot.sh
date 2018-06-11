b20b3a10ac6686cb5387d74c4432d71597f7de0c
Andy Chu <andy@oilshell.org>
2018-06-10 20:43:28 -0700

    got demo of proot working

--- test/proot.sh
+++ test/proot.sh
@@ -9,9 +9,18 @@ set -o errexit
 
 # permission problems
 demo() {
-  proot --rootfs=_chroot/proot1 /bin/ls --version
+  # Wow this is necessary to avoid segfault!
+  # https://github.com/proot-me/PRoot/issues/106
+  export PROOT_NO_SECCOMP=1
+
+  # 'whoami' shows unknown UID.
+  # --pwd avoids a warning.
+  proot --rootfs=_chroot/proot1 --pwd=/ /bin/sh -c 'whoami; ls /'
+
+  #proot --rootfs=_chroot/proot1 /bin/ls --version
 }
 
+# Copied from _chroot/alpine1.
 prepare() {
   sudo chown --verbose --recursive andy _chroot/proot1
 }
